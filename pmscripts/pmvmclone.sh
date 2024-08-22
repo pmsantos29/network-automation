bb5c82ac6253e8ba09ec26d92f769fb9e4c44436
SoloQueueTrash <escola.pedro.cunha@gmai.com>
2024-08-22 12:22:35 +0100

    proxmox vm management scrip updated

--- pmscripts/pmvmclone.sh
+++ pmscripts/pmvmclone.sh
@@ -5,58 +5,75 @@ snaptx=$3
 function usage () {
     echo
     echo "  - create virtual machines"
-    echo "    ./pmvectl.sh create {template-id} {first-clone-id} {last-clone-id}"
+    echo "    ./vm-manager.sh create {template-id} {first-clone-id} {hostnames-list-file}"
     echo "  - destroy virtual machines"
-    echo "    ./pmvectl.sh destroy {first-vm-id} {last-vm-id}"
+    echo "    ./vm-manager.sh destroy {first-vm-id} {last-vm-id}"
     echo "  - start virtual machines"
-    echo "    ./pmvectl.sh start {first-vm-id} {last-vm-id}"
+    echo "    ./vm-manager.sh start {first-vm-id} {last-vm-id}"
     echo "  - stop virtual machines"
-    echo "    ./pmvectl.sh stop {first-vm-id} {last-vm-id}"
+    echo "    ./vm-manager.sh stop {first-vm-id} {last-vm-id}"
     echo "  - rollback virtual machines"
-    echo "    ./pmvectl.sh rollback {first-vm-id} {last-vm-id}"
+    echo "    ./vm-manager.sh rollback {first-vm-id} {last-vm-id}"
     echo "  - get IP addresses of virtual machines"
-    echo "    ./pmvectl.sh get-ip {first-vm-id} {last-vm-id}"
-    echo 
+    echo "    ./vm-manager.sh get-ip {first-vm-id} {last-vm-id} {output-file}"
+    echo
 }
 
 case $1 in
     create)
+        if [ ! -f "$4" ]; then
+            echo "Error: Hostnames list file '$4' does not exist."
+            exit 1
+        fi
+
         echo
         echo -n "Creating virtual machines:"
-        for i in $(seq $3 $4); do
-            hostname=$(grep -w ${i} pmvmhosts.txt | cut -d ' ' -f 2)
-            qm clone $2 ${i} --name ${hostname} --full > /dev/null
+
+        initial_id=$3  # Starting VM ID
+        current_id=$initial_id
+
+        # Read the hostnames from hosts.txt and create VMs
+        while read -r hostname; do
+            qm clone $2 ${current_id} --name ${hostname} --full 
+            
             echo -n "."
 
             # Set memory
-            qm set ${i} --memory 4096
+            qm set ${current_id} --memory 4096
 
             # Set CPUs
-            qm set ${i} --sockets 1 --cores 2 --cpu cputype=kvm64
+            qm set ${current_id} --sockets 1 --cores 2 --cpu cputype=kvm64
 
             # Set disk size
-            qm resize ${i} scsi0 32G
+            qm resize ${current_id} scsi0 32G
 
             # Enable QEMU Guest Agent
-            qm set ${i} --agent enabled=1
+            qm set ${current_id} --agent enabled=1
 
             # Set networking
-            qm set ${i} --net0 virtio,bridge=vmbr0,firewall=1
-        done
+            qm set ${current_id} --net0 virtio,bridge=vmbr0,firewall=1
+
+            # Increment VM ID for the next VM
+            current_id=$((current_id + 1))
+        done < $4
         echo " done."
 
         echo -n "Snapshotting virtual machines:"
-        for i in $(seq $3 $4); do
-            qm snapshot ${i} snap01 --description "Initial snapshot."
+        current_id=$initial_id  # Reset to the initial ID for snapshotting
+        while read -r hostname; do
+            qm snapshot ${current_id} snap01 --description "Initial snapshot."
             echo -n "."
-        done
+            current_id=$((current_id + 1))
+        done < hosts.txt
         echo " done."
         ;;
+
+
     destroy)
         echo
         echo -n "Destroying virtual machines:"
         for i in $(seq $2 $3); do
-            qm destroy ${i} > /dev/null
+            qm destroy ${i} 
             echo -n "."
         done
         echo " done."
@@ -83,7 +100,7 @@ case $1 in
         echo
         echo -n "Rolling back virtual machines:"
         for i in $(seq $2 $3); do
-            qm rollback ${i} snap01 > /dev/null
+            qm rollback ${i} snap01 
             echo -n "."
         done
         echo " done."
@@ -94,7 +111,7 @@ case $1 in
         for i in $(seq $2 $3); do
             hostname=$(qm config ${i} | grep -i name | awk -F ": " '{print $2}')
             ip=$(qm guest exec ${i} -- ip -4 addr show ens18 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
-            echo "${i} ${hostname} ${ip}" >> pmvmips.txt
+            echo "${i} ${hostname} ${ip}" >> $4
             echo -n "."
         done
         echo " done. IP addresses saved to pmvmips.txt."
